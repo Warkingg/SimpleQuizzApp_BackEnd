31b06d284e66a904e9c420a63b7b7848759e41db
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-05-26 09:51:29 +0700

    add user role entity

---
+++ src/main/java/com/example/quizztest/model/UserRole.java
@@ -0,0 +1,23 @@
+package com.example.quizztest.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Role role;
+
+    private String name;
+
+    private String admin;
+}
