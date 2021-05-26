53caa889e5d84ed84fd696c6e8990a194217a82b
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-05-26 09:55:31 +0700

    add user repository

---
+++ src/main/java/com/example/quizztest/repository/IUserRepository.java
@@ -0,0 +1,13 @@
+package com.example.quizztest.repository;
+
+import com.example.quizztest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+    User findByEmail(String email);
+}
